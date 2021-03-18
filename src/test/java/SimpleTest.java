297e942967c1ae7c67f9b0fd3167e7bce7de0775
kylosova_iuliia <sirintegral1991@gmail.com>
2021-03-18 21:58:01 +0300

    added SimpleTest

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleTest {
+    @Test
+    public void test_positive_scenarios(){
+        assertEquals(true, true);
+    };
+}
